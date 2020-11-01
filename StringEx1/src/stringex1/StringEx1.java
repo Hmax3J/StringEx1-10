da04b48c91e922bc13e04224de74e99f795cb7c8
Hmax3J <Hmax3J@gamil.com>
2020-11-02 01:09:17 +0900

    1

---
+++ StringEx1/src/stringex1/StringEx1.java
@@ -0,0 +1,13 @@
+package stringex1;
+import java.io.*;
+
+public class StringEx1 {
+
+	public static void main(String[] args) throws IOException {
+		
+		int a = System.in.read();
+		System.out.print(a);
+
+	}
+
+}
